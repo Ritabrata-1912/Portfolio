3a783e3e47c0e259b93b0951252d13c93d00bbb3
Ritabrata-1912 <99819504+Ritabrata-1912@users.noreply.github.com>
2023-07-02 17:23:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,4 +35,14 @@ window.onscroll = () => {
 
 };
 
+//typed js
+
+const typed= new Typed('.multiple-text',{
+  strings : ['FrontEnd Developer', 'Software Engineer','Problem Solver'],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+});
+
 
